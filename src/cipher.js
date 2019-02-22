efb20f54f503d045254d4dad62d6399ca617ee4d
Gianella Vargas <gianella.vq.copernico@gmail.com>
2019-02-22 10:57:56 -0600

    cifrado terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,14 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (pass, offset) => {
+    let newPass = '';
+    for (let i = 0; i < pass.length; i++) {
+      let indice = pass[i].charCodeAt();
+      let numAsci = (((indice - 65) + offset) % 26 + 65);
+      let letterAsci = String.fromCharCode(numAsci);
+      newPass += letterAsci;
+      resultBox.textContent = newPass;
+      console.log(newPass); 
+    }
   },
   decode: () => {
     /* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-/* Acá va tu código */
+let formEncode = document.getElementById('form-encode'),
+pass = document.getElementById('pass'),
+offset = document.getElementById('offset'),
+resultBox = document.getElementById('result');
+
+formEncode.addEventListener('submit', e =>{
+    e.preventDefault();
+    pass = e.target.pass.value;
+    offset = parseInt(e.target.offset.value);
+    // console.log(pass, offset); // comprobando que si me captura correctamente los datos
+    cipher.encode(pass, offset);
+    // resultBox.textContent = pass;
+    
+});
